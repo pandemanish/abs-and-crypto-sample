c78e29daca7ab7cc5c7d8f6dd23a793dd4220fbb
pandemanish <pande9970666615@gmail.com>
2013-10-26 20:49:21 +0530

    Displaying a ListView with the Files in the Present Derectory

---
+++ src/com/abstrucelogic/filesec/FileListActivity.java
@@ -0,0 +1,200 @@
+package com.abstrucelogic.filesec;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * 
+ * @author MANISH
+ *
+ */
+public class FileListActivity extends Activity {
+
+	public final static String FILE_PATH = "file_path";
+	public final static String ALLOW_HIDDEN_FILES = "show_hidden_files";
+	public final static String ACCEPTED_FILE_EXTENSIONS = "accepted_file_extensions";
+	private final static String DEFAULT_INITIAL_DIRECTORY = "/";
+	public static String Path ="";
+	protected File mDirectory;
+	protected ArrayList<File> mFiles;
+	protected FileListAdapter mAdapter;
+	private ListView listView;
+	private Button button;
+	private TextView pathTextView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Keeping the Orientation PORTRAIT
+		setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); 
+		//remove the title bar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//To Make app full screen
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+		//setting UI from XML 
+		setContentView(R.layout.file_picker_activity);
+
+		listView = (ListView)findViewById(R.id.fileList);
+		button =(Button)findViewById(R.id.setPathButton);
+		pathTextView =(TextView)findViewById(R.id.pathTextView);
+		// Set the view to be shown if the list is empty
+		LayoutInflater inflator = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View emptyView = inflator.inflate(R.layout.file_picker_empty_view, null);
+		((ViewGroup) listView.getParent()).addView(emptyView);
+		listView.setEmptyView(emptyView);
+
+		// Set initial directory
+		mDirectory = new File(DEFAULT_INITIAL_DIRECTORY);
+
+		// Initialize the ArrayList
+		mFiles = new ArrayList<File>();
+
+		// Set the ListAdapter
+		mAdapter = new FileListAdapter(this, mFiles);
+		listView.setAdapter(mAdapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> view, View v, int position,
+					long id) {
+				File nfile = (File)view.getItemAtPosition(position);
+
+				if(nfile.isFile()) {
+					//File Encripe hear
+					// Finish the activity
+					finish();
+				} else {
+					mDirectory = nfile;
+					// Update the files list
+					refList();
+				}
+			}
+		});
+
+
+		// Get intent extras
+		if(getIntent().hasExtra(FILE_PATH)) {
+			mDirectory = new File(getIntent().getStringExtra(FILE_PATH));
+		}
+		
+		button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				if(mDirectory.getParentFile() != null) {
+					// Go to parent directory
+					mDirectory = mDirectory.getParentFile();
+					refList();
+					return;
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onResume() {
+		refList();
+		super.onResume();
+	}
+
+	/**
+	 * Updates the list view to the current directory
+	 */
+	protected void refList() {
+
+		// Clear the files ArrayList
+		mFiles.clear();
+		pathTextView.setText(mDirectory.getAbsolutePath());
+		// Get the files in the directory
+		File[] files = mDirectory.listFiles();
+		if(files != null && files.length > 0) {
+			for(File f : files) {
+				if(f.isHidden()) {
+					// Don't add the file
+					continue;
+				}
+				// Add the file the ArrayAdapter
+				mFiles.add(f);
+			}
+
+			Collections.sort(mFiles);
+		}
+		mAdapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onBackPressed() {
+		if(mDirectory.getParentFile() != null) {
+			// Go to parent directory
+			mDirectory = mDirectory.getParentFile();
+			refList();
+			return;
+		}
+
+		super.onBackPressed();
+	}
+
+
+
+	private class FileListAdapter extends ArrayAdapter<File> {
+
+		private List<File> mObjects;
+
+		public FileListAdapter(Context context, List<File> objects) {
+			super(context, R.layout.file_picker_list_item, android.R.id.text1, objects);
+			mObjects = objects;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			View row = null;
+
+			if(convertView == null) { 
+				LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				row = inflater.inflate(R.layout.file_picker_list_item, parent, false);
+			} else {
+				row = convertView;
+			}
+
+			File object = mObjects.get(position);
+
+			ImageView imageView = (ImageView)row.findViewById(R.id.file_picker_image);
+			TextView textView = (TextView)row.findViewById(R.id.file_picker_text);
+			// Set single line
+			textView.setSingleLine(true);
+
+			textView.setText(object.getName());
+			if(object.isFile()) {
+				// Show the file icon
+				imageView.setImageResource(R.drawable.file);
+			} else {
+				// Show the folder icon
+				imageView.setImageResource(R.drawable.folder);
+			}
+
+			return row;
+		}
+
+	}
+}
--- src/com/abstrucelogic/filesec/HomeActivity.java
+++ src/com/abstrucelogic/filesec/HomeActivity.java
@@ -1,5 +1,5 @@
 package com.abstrucelogic.filesec;
-
+ 
 import com.abstrucelogic.filesec.R;
 import com.abstrucelogic.filesec.view.FileListItem;
 
@@ -18,42 +18,44 @@ import android.widget.Toast;
 public class HomeActivity extends Activity implements OnItemClickListener {
 
 	private ListView mFileListView;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        init();
-    }
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		init();
+		
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-    
-    @Override
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		Toast.makeText(this, "item tapped", Toast.LENGTH_SHORT).show();
 	}
-    
-    private void init() {
-    	mFileListView = (ListView) this.findViewById(R.id.file_list);
-    	mFileListView.setAdapter(new FileListAdapter(this));
-    	mFileListView.setOnItemClickListener(this);
-    }
-    
-    private class FileListAdapter extends BaseAdapter {
 
-    	private Context mCurContext;
-    	
-    	public FileListAdapter(Context context) {
-    		this.mCurContext = context;
-    	}
-    	
+	private void init() {
+		mFileListView = (ListView) this.findViewById(R.id.file_list);
+		mFileListView.setAdapter(new FileListAdapter(this));
+		mFileListView.setOnItemClickListener(this);
+	}
+
+	private class FileListAdapter extends BaseAdapter {
+
+		private Context mCurContext;
+
+		public FileListAdapter(Context context) {
+			this.mCurContext = context;
+		}
+
 		@Override
 		public int getCount() {
+			
 			return 2;
 		}
 
@@ -64,7 +66,6 @@ public class HomeActivity extends Activity implements OnItemClickListener {
 
 		@Override
 		public long getItemId(int position) {
-			// TODO Auto-generated method stub
 			return 0;
 		}
 
@@ -76,8 +77,9 @@ public class HomeActivity extends Activity implements OnItemClickListener {
 			} else {
 				listItem = (FileListItem) convertView;
 			}
+			listItem.setName("");
 			return listItem;
 		}
-    	
-    }
+
+	}
 }
